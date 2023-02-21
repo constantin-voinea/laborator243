65591fcaa6ca3a83f146efd42b163cd14b55ba4c
koss.daniel <cos.voinea@gmail.com>
2023-02-21 11:31:20 +0200

    cerinte proiect laborator

---
+++ src/main/java/laboratoare/Main.java
@@ -0,0 +1,10 @@
+package main.java.laboratoare;
+
+/**
+ * @author cvoinea
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
